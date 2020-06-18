8402821dd37b264e0a7d1f009a6a3ad3e8b72617
ikhsanmahendri <ikhsan.mahendri@gmail.com>
2020-06-18 07:51:38 +0700

    change dir container and content

--- src/components/Container_/index.js
+++ src/components/Container_/index.js
@@ -6,7 +6,7 @@ const Container = styled.div`
     display: flex;
     flex-direction: column;
     margin: 0 auto;
-    padding: ${props => (props.noPadding ? 0 : "20px 15px 10px 15px")};
+    padding: ${(props) => (props.noPadding ? 0 : "20px 15px 10px 15px")};
     padding-bottom: 20px;
     width: 1024px;
     @media (max-width: 1024px) {
